b1c525308b4fbad5048083503c39ac7d4682684e
seeya <s10094128@alumni.np.edu.sg>
2020-09-06 06:58:08 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+  "github.com/skip2/go-qrcode"
+)
+
+func main() {
+  fmt.Println("TextToQR Service Up")
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "shoo shoo")
+  })
+
+  http.HandleFunc("/qr", func(w http.ResponseWriter, r *http.Request) {
+    keys, ok := r.URL.Query()["text"]
+    
+    if !ok || len(keys[0]) < 1 {
+        log.Println("Url Param 'text' is missing")
+        return
+    }
+
+    key := keys[0]
+
+    log.Println("Url Param 'text' is: " + string(key))    
+
+    var png []byte
+    fmt.Println(r)
+    png, err := qrcode.Encode(string(key), qrcode.Medium, 256)
+
+    if err != nil {} 
+    w.Write(png)
+  })
+  
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
